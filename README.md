178f81e0be8eaf743fe50465c3467741e682465a
Chakradhar Raju <chakradhar@murmuration.in>
2021-11-21 23:14:41 +0530

    adding server and client v2

---
+++ README.md
@@ -0,0 +1,1 @@
+MERN app to upload memes and memories.
---
+++ client/src/App.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from "./components/images/meme1.jpg";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts";
+
+const App = () => {
+  const [currentId, setCurrentId] = useState(null);
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <img className={classes.image} src={memories} alt="meme" height="60" />
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memeory
+        </Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+            
+            className={classes.mainContainer}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
